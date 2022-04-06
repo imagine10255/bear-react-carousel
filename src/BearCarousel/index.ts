57fe3839099afe746894741eb4abe6d8992f0cfa
imagine10255 <imagine10255@gmail.com>
2022-04-06 15:18:44 +0800

    fix: fix nextjs import test v2

--- src/BearCarousel/index.ts
+++ src/BearCarousel/index.ts
@@ -1,5 +1,4 @@
 export {default} from './BearCarousel';
 export {default as elClassName} from './el-class-name';
 export {default as BearSlideItem} from './BearSlideItem';
-export {default as BearCarousel} from './BearCarousel';
 export * from './types';
--- src/index.ts
+++ src/index.ts
@@ -1,4 +1,10 @@
-export {default} from './BearCarousel';
-export {default as elClassName} from './BearCarousel/el-class-name';
-export {default as BearSlideItem} from './BearCarousel/BearSlideItem';
+import BearSlideContainer from './BearCarousel';
+import BearSlideItem from './BearCarousel/BearSlideItem';
+import elClassName from './BearCarousel/el-class-name';
+
 export * from './BearCarousel/types';
+export default {
+    BearSlideContainer,
+    BearSlideItem,
+    elClassName,
+}
